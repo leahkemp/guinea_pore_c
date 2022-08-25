af6a3d3985e5d6b0f0a5f2369f7e5e6bda399bb0
Leah Kemp <leah.kemp@esr.cri.nz>
2022-08-25 12:44:47 +1200

    un-comment code

--- scripts/04_pore_c_snakemake_pipeline_run/02_pore_c_snakemake_pipeline_run.sh
+++ scripts/04_pore_c_snakemake_pipeline_run/02_pore_c_snakemake_pipeline_run.sh
@@ -12,17 +12,17 @@
 project_dir="/NGS/scratch/KSCBIOM/HumanGenomics/guinea_pore_c"
 
 # remove any old outputs of this script to avoid results being written twice to a file
-#rm -rf $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
+rm -rf $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
 
 # get pore-c snakemake pipeline
 echo ""
 echo "Getting pore-c snakemake pipeline"
 echo ""
 
-#git clone https://github.com/nanoporetech/Pore-C-Snakemake.git $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
-#cd $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
-#git checkout tags/0.3.0
-#cd $project_dir/
+git clone https://github.com/nanoporetech/Pore-C-Snakemake.git $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
+cd $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
+git checkout tags/0.3.0
+cd $project_dir/
 
 # set the shell to be used by conda for this script (and re-start shell to implement changes)
 echo ""
@@ -33,32 +33,32 @@ conda init bash
 source ~/.bashrc
 
 # create conda environment with pipeline dependencies installed and activate it
-#echo ""
-#echo "Creating conda environment with pipeline dependencies installed"
-#echo ""
+echo ""
+echo "Creating conda environment with pipeline dependencies installed"
+echo ""
 
 #mamba env create --force -f $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/environment.yml
 conda activate pore-c-snakemake
 
 # generate pore-c snakemake pipeline DAG
-#echo ""
-#echo "Generating pore-c snakemake pipeline DAG"
-#echo ""
+echo ""
+echo "Generating pore-c snakemake pipeline DAG"
+echo ""
 
 cd $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/
 
-#snakemake \
-#--configfile $project_dir/config/04_pore_c_snakemake_pipeline_run/config.yaml \
-#--dag | dot -Tpng > $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/pipeline_dag.png
+snakemake \
+--configfile $project_dir/config/04_pore_c_snakemake_pipeline_run/config.yaml \
+--dag | dot -Tpng > $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/pipeline_dag.png
 
 # generate pore-c snakemake pipeline rulegraph
-#echo ""
-#echo "Generating pore-c snakemake pipeline rulegraph"
-#echo ""
+echo ""
+echo "Generating pore-c snakemake pipeline rulegraph"
+echo ""
 
-#snakemake \
-#--configfile $project_dir/config/04_pore_c_snakemake_pipeline_run/config.yaml \
-#--rulegraph | dot -Tpng > $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/pipeline_rulegraph.png
+snakemake \
+--configfile $project_dir/config/04_pore_c_snakemake_pipeline_run/config.yaml \
+--rulegraph | dot -Tpng > $project_dir/results/04_pore_c_snakemake_pipeline_run/Pore-C-Snakemake/pipeline_rulegraph.png
 
 # run pore-c snakemake pipeline
 echo ""
